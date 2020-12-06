5c1ba338f4eca1698c0af082911df27994d17789
Marco Paesano <60313204+smarambas@users.noreply.github.com>
2020-12-06 12:36:28 +0100

    Removed unused libraries

--- src/main/java/standalone_view/GUIController.java
+++ src/main/java/standalone_view/GUIController.java
@@ -1,11 +1,8 @@
 package standalone_view;
 
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;

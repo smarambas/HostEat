d0450cb4b2bfa73cd2da0efb2c8a300c1210403d
Marco Paesano <60313204+smarambas@users.noreply.github.com>
2021-01-07 18:43:33 +0100

    Wrote all crud and simple queries, added two new exceptions and new DAOs

--- src/main/java/model/dao/UserDAO.java
+++ src/main/java/model/dao/UserDAO.java
@@ -5,7 +5,10 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import exceptions.DuplicateRecordException;
+import exceptions.NoRecordFoundException;
 import model.User;
+import model.queries.CRUDQueries;
 import model.queries.SimpleQueries;
 import model.Usertype;
 
@@ -15,7 +18,7 @@ public class UserDAO {
 	
 	private UserDAO() {}
 	
-	public static List<User> retrieveUserByUsername(String username) {
+	public static List<User> retrieveUserByUsername(String username) throws Exception {
 		Statement stm = null;
 		List<User> userList = new ArrayList<User>();
 		
@@ -48,13 +51,17 @@ public class UserDAO {
 				Usertype type = Usertype.valueOf(rs.getString("type").toUpperCase());
 				
 				User newUser = new User(type, usern, passw, name, surname, email);
+				newUser.setSex(sex);
+				newUser.setRegion(region);
+				newUser.setProvince(province);
+				newUser.setCity(city);
+				newUser.setAddress(address);
+				newUser.setAge(age);
 				
 				userList.add(newUser);
 			}while(rs.next());
 			
 			rs.close();
-		} catch (Exception e) {
-			e.printStackTrace();
 		} finally {
 			try {
 				if(stm != null) {
@@ -66,6 +73,72 @@ public class UserDAO {
 		}
 		
 		return userList;
+	}
+	
+	public static void saveUser(User user) throws Exception {
+		Statement stm = null;
+		
+		try {
+			cs = ConnectionSingleton.createConnection();
+			
+			stm = cs.getConnection().createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY);
+			
+			ResultSet rs = SimpleQueries.selectUserByUsername(stm, user.getUsername());
+			
+			if(rs.next()) {
+				throw new DuplicateRecordException("ERROR: the user already exists");
+			}
+			else {
+				rs.close();
+				stm.close();
+				stm = cs.getConnection().createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+						ResultSet.CONCUR_READ_ONLY);
+				
+				int result = CRUDQueries.insertUser(stm, user);
+			}
+		} finally {
+			try {
+				if(stm != null) {
+					stm.close();
+				}
+			} catch(SQLException sqle) {
+				
+			}
+		}
+	}
+	
+	public static void removeUser(User user) throws Exception {
+		Statement stm = null;
+		
+		try {
+			cs = ConnectionSingleton.createConnection();
+			
+			stm = cs.getConnection().createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY);
+			
+			ResultSet rs = SimpleQueries.selectUserByUsername(stm, user.getUsername());
+			
+			if(rs.next()) {
+				rs.close();
+				stm.close();
+				stm = cs.getConnection().createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+						ResultSet.CONCUR_READ_ONLY);
+				
+				int result = CRUDQueries.deleteUser(stm, user);
+			}
+			else {
+				throw new NoRecordFoundException("ERROR: no record found");
+			}
+		} finally {
+			try {
+				if(stm != null) {
+					stm.close();
+				}
+			} catch(SQLException sqle) {
+				
+			}
+		}
 	}
 	
 }
--- src/main/java/model/queries/SimpleQueries.java
+++ src/main/java/model/queries/SimpleQueries.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class SimpleQueries {
-
+	
 	private SimpleQueries() {}
 	
 	public static ResultSet selectUserByUsername(Statement stmt, String username) throws SQLException {
@@ -13,4 +13,33 @@ public class SimpleQueries {
 		return stmt.executeQuery(statement);
 	}
 	
+	public static ResultSet selectAllergiesByUsername(Statement stmt, String username) throws SQLException {
+		String statement = "SELECT name FROM allergy WHERE user = '" + username + "';";
+		return stmt.executeQuery(statement);
+	}
+	
+	public static ResultSet selectCuisinesByUsername(Statement stmt, String username) throws SQLException {
+		String statement = "SELECT name FROM cuisine WHERE user = '" + username + "';";
+		return stmt.executeQuery(statement);
+	}
+	
+	public static ResultSet selectEventByUsername(Statement stmt, String username) throws SQLException {
+		String statement = "SELECT * FROM event WHERE owner = '" + username + "';";
+		return stmt.executeQuery(statement);
+	}
+	
+	public static ResultSet selectEventByDate(Statement stmt, String username, String date) throws SQLException {
+		String statement = "SELECT * FROM event WHERE owner = '" + username + "' AND date = '" + date + "';";
+		return stmt.executeQuery(statement);
+	}
+	
+	public static ResultSet selectEventByDateTime(Statement stmt, String username, String date, String time) throws SQLException {
+		String statement = "SELECT * FROM event WHERE owner = '" + username + "' AND date = '" + date + "' AND time = '" + time + "';";
+		return stmt.executeQuery(statement);
+	}
+	
+	public static ResultSet selectMenuByUsernameDateTime(Statement stmt, String username, String date, String time) throws SQLException {
+		String statement = "SELECT name, course FROM dish WHERE event_owner = '" + username + "' AND event_date = '" + date + "' AND event_time = '" + time + "';";
+		return stmt.executeQuery(statement);
+	}
 }
